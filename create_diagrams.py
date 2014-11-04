bff4f61eb61a850fc91f40b02e3ac0109f96ba31
Daniel Wutke <dwu.public@googlemail.com>
2014-11-04 20:02:14 +0100

    Diagram creation refactored and extended
    Added decompression diagram.

--- create_diagrams.py
+++ create_diagrams.py
@@ -6,10 +6,25 @@ from pandas import *
 from matplotlib.ticker import FuncFormatter
 import matplotlib.pyplot as plt
 
-def gen_quot(row):
-	row['quot'] = row['compsize'] / row['duration']
-
-df = read_csv("results.csv", sep=",")
+# functions
+def create_diagram(df, title, filename, y_axis_value, y_axis_label, colors):
+	color = 0
+	fig = plt.figure(figsize=(10,6))
+	title = fig.suptitle(title, fontsize=18)
+	for key, group in df.groupby('compressor'):
+		ax = group.plot(x='method', y=y_axis_value, label=key, color=colors[color], linewidth=3, kind='line', marker='o')
+		ax.xaxis.set_tick_params(pad=15)
+		ax.yaxis.set_tick_params(pad=15)
+		ax.yaxis.set_major_formatter(FuncFormatter(lambda x, pos: x / 1000000))
+		ax.set_ylabel(y_axis_label, fontsize=14)
+		ax.set_xlabel("Compression level", fontsize=14)
+		color += 2
+	
+	for label in ax.get_xticklabels() + ax.get_yticklabels():
+		label.set_fontsize(12)
+	
+	lgd = plt.legend(loc='center left', bbox_to_anchor=(1.02, 0.5))
+	fig.savefig(filename, bbox_extra_artists=(lgd,title), bbox_inches='tight')
 
 # colors
 tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
@@ -22,78 +37,16 @@ for i in range(len(tableau20)):
     r, g, b = tableau20[i]
     tableau20[i] = (r / 255., g / 255., b / 255.)
 
-## duration figure
-color = 0
-fig = plt.figure(figsize=(10,6))
-fig.suptitle("Duration", fontsize=18)
-for key, group in df.groupby('compressor'):
-    ax = group.plot(x='method', y='duration', label=key, color=tableau20[color], linewidth=3, kind='line', marker='o')
-    ax.xaxis.set_tick_params(pad=15)
-    ax.yaxis.set_tick_params(pad=15)
-    ax.yaxis.set_major_formatter(FuncFormatter(lambda x, pos: x / 1000))
-    ax.set_ylabel("Duration (s)", fontsize=14)
-    ax.set_xlabel("Compression level", fontsize=14)
-    color += 2
+## import and process compression csv
+df = read_csv("results_compress.csv", sep=",")
+df['ratio'] = df['compsize'] / df['origsize']
+df['time_efficiency'] = (df['origsize'] / df['compsize']) / df['duration']
+create_diagram(df, "Compressed size (bytes)", "compression_size.png", "compsize", "Compressed size (mb)", tableau20)
+create_diagram(df, "Compression Ratio", "compression_ratio.png", "ratio", "Compression ratio", tableau20)
+create_diagram(df, "Comp. Time efficiency ((origsize / compsize) / duration)", "comp_time_efficiency.png", "time_efficiency", "Compression time efficiency", tableau20)
+create_diagram(df, "Compression duration (s)", "compression_duration.png", "duration", "Compression duration (s)", tableau20)
 
-for label in ax.get_xticklabels() + ax.get_yticklabels():
-    label.set_fontsize(12)
+## import and process decompression csv
+df = read_csv("results_decompress.csv", sep=",")
+create_diagram(df, "Decompression Duration(s)", "decompression_duration.png", "duration", "Decompression duration (s)", tableau20)
 
-lgd = plt.legend(loc='center left', bbox_to_anchor=(1.02, 0.5))
-fig.savefig('duration.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
-
-## compsize figure
-color = 0
-fig = plt.figure(figsize=(10,6))
-fig.suptitle("Compressed size (bytes)", fontsize=18)
-for key, group in df.groupby('compressor'):
-    ax = group.plot(x='method', y='compsize', label=key, color=tableau20[color], linewidth=3, kind='line', marker='o')
-    ax.xaxis.set_tick_params(pad=15)
-    ax.yaxis.set_tick_params(pad=15)
-    ax.yaxis.set_major_formatter(FuncFormatter(lambda x, pos: x / 1000000))
-    ax.set_ylabel("Compressed size (mb)", fontsize=14)
-    ax.set_xlabel("Compression level", fontsize=14)
-    color += 2
-
-for label in ax.get_xticklabels() + ax.get_yticklabels():
-    label.set_fontsize(12)
-
-lgd = plt.legend(loc='center left', bbox_to_anchor=(1.02, 0.5))
-fig.savefig('compsize.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
-
-## ratio figure
-color = 0
-fig = plt.figure(figsize=(10,6))
-fig.suptitle("Compression Ratio", fontsize=18)
-for key, group in df.groupby('compressor'):
-    ax = group.plot(x='method', y='ratio', label=key, color=tableau20[color], linewidth=3, kind='line', marker='o')
-    ax.xaxis.set_tick_params(pad=15)
-    ax.yaxis.set_tick_params(pad=15)
-    ax.yaxis.get_major_formatter().set_scientific(False)
-    ax.set_ylabel("Compression Ratio", fontsize=14)
-    ax.set_xlabel("Compression level", fontsize=14)
-    color += 2
-
-for label in ax.get_xticklabels() + ax.get_yticklabels():
-    label.set_fontsize(12)
-
-lgd = plt.legend(loc='center left', bbox_to_anchor=(1.02, 0.5))
-fig.savefig('ratio.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
-
-## time_efficiency figure
-color = 0
-fig = plt.figure(figsize=(10,6))
-fig.suptitle("Time efficiency ((origsize / compsize) / duration)", fontsize=18)
-for key, group in df.groupby('compressor'):
-    ax = group.plot(x='method', y='time_efficiency', label=key, color=tableau20[color], linewidth=3, kind='line', marker='o')
-    ax.xaxis.set_tick_params(pad=15)
-    ax.yaxis.set_tick_params(pad=15)
-    ax.yaxis.get_major_formatter().set_scientific(False)
-    ax.set_ylabel("Time efficiency ((origsize / compsize) / duration)", fontsize=14)
-    ax.set_xlabel("Compression level", fontsize=14)
-    color += 2
-
-for label in ax.get_xticklabels() + ax.get_yticklabels():
-    label.set_fontsize(12)
-
-lgd = plt.legend(loc='center left', bbox_to_anchor=(1.02, 0.5))
-fig.savefig('time_efficiency.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
